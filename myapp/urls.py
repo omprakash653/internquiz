d30444a50a57668bc0a615dbb9908cfeb30893af
unknown <shiv.ompr97@gmail.com>
2022-07-15 19:22:41 +0530

    edit readme and add new.md

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -12,6 +14,10 @@ urlpatterns = [
     path('submit-answer/<int:cat_id>/<int:quest_id>',
          views.submit_answer, name='submit_answer'),
     path('attempt-limit/', views.attempt_limit, name='attempt-limit'),
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
